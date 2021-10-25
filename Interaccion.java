db187c1c4429f9873dd73e0569aa41cc7012745a
Samuel Argueta <arg211024@uvg.edu.gt>
2021-10-24 19:34:14 -0600

    Carga de UML modificado

--- Interaccion.java
+++ Interaccion.java
@@ -65,28 +65,28 @@ public class Interaccion {
         System.out.println("3. Donativo monetario.");
         System.out.println("4. Salir");
     }
+
     /**
      * Método para desplegar las opciones a donar para alimentos
      */
 
-    private void menu_alimentos(){
+    private void menu_alimentos() {
         String alimentos_esenciales = "-Frijol\n -Arroz\n -Aceite\n -Harina\n -Sal\n -Azúcar\n -Pasta\n";
         String otros_alimentos = "-Enlatados\n -Cereales\n -Otros";
-        String advertencia = "POR FAVOR TOMAR EN CUENTA DE NO DONAR LÁCTEOS, ALIMENTOS PERECEDEROS," + "PRODUCTOS DE GRANJA O ENVASADOS EN " + 
-        "ENVASES DE VIDRIO";
+        String advertencia = "POR FAVOR TOMAR EN CUENTA DE NO DONAR LÁCTEOS, ALIMENTOS PERECEDEROS,"
+                + "PRODUCTOS DE GRANJA O ENVASADOS EN " + "ENVASES DE VIDRIO";
         System.out.println(alimentos_esenciales);
         System.out.println(otros_alimentos);
         System.out.println(advertencia);
     }
-    
+
     /**
      * Método para desplegar productos de aseo ya sea personal o doméstico
      */
 
-    private void productos_de_aseo(){
-        String productos_aseo = "-Pasta dental\n -Papel higénico\n -Pañales\n -Shampoo\n -Jabón\n -Desodorante\n ";
+    private void productos_de_aseo() {
+        String productos_aseo = "- Pasta dental\n -Papel higénico\n -Pañales\n -Shampoo\n -Jabón\n -Desodorante\n ";
     }
-
 
     /**
      * Método que sirve para poder obtener un valor correcto por parte del usuario
@@ -131,15 +131,14 @@ public class Interaccion {
         boolean correcto = false;
         int selecto = 0;
         String[] datos = new String[3];
-        //por si se hace alimento
+        // por si se hace alimento
         if (valor.equals("comida")) {
 
             menu_alimentos();
             datos[0] = JOptionPane.showInputDialog("Ingrese qué alimento desea donar");
-            
-        //por si es vestimenta
-        } else if (valor.equals("ropa")) {
 
+            // por si es vestimenta
+        } else if (valor.equals("ropa")) {
 
             datos[0] = JOptionPane.showInputDialog("Ingrese el tipo de prenda que se va a donar");
 
@@ -155,9 +154,9 @@ public class Interaccion {
                 }
             } while (!correcto);
 
-        //por si se hace monetario
+            // por si se hace monetario
         } else if (valor.equals("dinero")) {
-            
+
             datos[0] = JOptionPane.showInputDialog("Ingrese su tarjeta");
             do {
                 try {
